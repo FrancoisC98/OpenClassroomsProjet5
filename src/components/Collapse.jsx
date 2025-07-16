bd16a13f18148f4e07e1fa69a01ee44c6aae0bef
François <francoiscombaud17@gmail.com>
2025-07-16 22:24:18 +0200

    Création page A propos avec Collapse

---
+++ src/components/Collapse.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import "../styles/Collapse.scss"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faChevronUp } from "@fortawesome/free-solid-svg-icons"
+
+
+function Collapse({ title, content}) {
+    const [isOpen, setIsOpen] = useState(false)
+    const toggleCollapse = () => {
+        setIsOpen(!isOpen);
+    }
+
+return (
+    <div className="collapse">
+        <div className="collapse-header" onClick={toggleCollapse}>
+            <h3>{title}</h3>
+            <FontAwesomeIcon icon={faChevronUp} className={`arrow ${isOpen ? "open" : ""}`} 
+                />
+        </div>
+    
+
+    {isOpen && (
+        <div className="collapse-content">
+            {Array.isArray(content) ? (
+                content.map ((item, index) => <p key={index}>{item}</p>)
+            ) : (
+                <p>{content}</p>
+            )}
+        </div>
+    )}
+    </div>
+);
+}
+
+export default Collapse
